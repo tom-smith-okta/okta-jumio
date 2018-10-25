d713db1f0b05cfeff105a96cacb3f04a60080edf
tom-smith-okta <tom.smith@okta.com>
2018-10-25 18:46:58 -0400

    add thank you page

--- app.js
+++ app.js
@@ -121,6 +121,8 @@ app.get('/callback', function (req, res) {
 
 app.post('/reg', function (req, res) {
 
+	res.session.email = req.body.email
+
 	console.log("the /reg email is: " + req.body.email)
 
 	var options = {
@@ -149,7 +151,44 @@ app.post('/reg', function (req, res) {
 	  console.log(body)
 
 	  console.log("the user id is: " + body.id)
+
+		var request = require("request");
+
+		var options = {
+			method: 'POST',
+			url: 'https://okta-jumio.oktapreview.com/api/v1/users/' + body.id + '/lifecycle/activate',
+			qs: { sendEmail: 'true' },
+			headers: {
+				'Cache-Control': 'no-cache',
+				Authorization: 'SSWS 00yigkWqw6xJo1IakrJt2CrvYEWbz6gMw1hq4zZJhp',
+				Accept: 'application/json',
+				'Content-Type': 'application/json'
+			}
+		};
+
+		request(options, function (error, response, body) {
+		  if (error) throw new Error(error);
+
+		  console.log(body);
+
+		  res.redirect('/thank_you')
+
+		});
 	});
+})
+
+app.get('/thank_you', function (req, res) {
+	fs.readFile('html/thank_you.html', (err, data) => {
+		if (err) {
+			console.log("error reading the thank_you.html file")
+		}
+
+		var page = data.toString()
+
+		page = page.replace(/{{email}}/g, res.session.email)
+
+		res.send(page)
+	})
 })
 
 app.post('/register', function (req, res) {
@@ -189,81 +228,4 @@ app.post('/register', function (req, res) {
 	  res.redirect(body.redirectUrl);
 
 	});
-})
-
-app.get('/checkAsset', function (req, res) {
-
-	var assetID = req.query.assetID
-
-	console.log("the assetID is: " + assetID)
-
-	// perform a check here to see if the asset id requires MFA.
-	// for this demo we are going to assume "yes"
-
-	var state = "assetID_" + assetID
-
-	var claims = {
-		"iss": process.env.OKTA_MFA_CLIENT_ID,
-		"exp": null,
-		"aud": "https://" + process.env.OKTA_TENANT,
-		"sub": "",
-		"response_type": "id_token",
-		"response_mode": "fragment",
-		"client_id": process.env.OKTA_MFA_CLIENT_ID,
-		"redirect_uri": process.env.OKTA_REDIRECT_URI,
-		"scope": "openid",
-		"acr_values": "urn:okta:app:mfa:attestation",
-		"state": state,
-		"nonce": "h3lX29$sWGxK3", // This should be generated dynamically
-		"login_hint": process.env.OKTA_MFA_USER
-	}
-
-	var jwt = nJwt.create(claims, process.env.OKTA_MFA_CLIENT_SECRET)
-
-	jwt.setExpiration(new Date().getTime() + (30*1000)) // thirty seconds from now
-
-	console.log(jwt)
-
-	var token = jwt.compact()
-	console.log(token)
-
-	var url = "https://" + process.env.OKTA_TENANT + "/oauth2/v1/authorize?request=" + token
-
-	console.log("\nthe redirect URL is:\n" + url + "\n")
-
-	res.redirect(url)
-})
-
-app.post('/getAsset', function(req, res) {
-
-	var options = {
-		method: 'POST',
-		url: 'https://' + process.env.OKTA_TENANT + '/oauth2/v1/introspect',
-		qs: {
-			token: req.body.id_token,
-			token_type_hint: 'id_token',
-			client_id: process.env.OKTA_MFA_CLIENT_ID,
-			client_secret: process.env.OKTA_MFA_CLIENT_SECRET
-		},
-		headers: {
-			'Cache-Control': 'no-cache',
-			'Content-Type': 'application/x-www-form-urlencoded',
-			Accept: 'application/json'
-		}
-	}
-
-	request(options, function (error, response, body) {
-		if (error) throw new Error(error)
-
-		var obj = JSON.parse(body)
-
-		console.log("the response from /introspect is: ")
-		console.dir(body)
-
-		if (obj.active === true) {
-			var imgURL = "/img/gameboy.jpg"
-
-			res.json({imgURL: imgURL})
-		}
-	})
 })
