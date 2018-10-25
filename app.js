334a7d098c6ecd78a0d8c1c6f2210daf9e3d2d05
tom-smith-okta <tom.smith@okta.com>
2018-10-25 18:22:07 -0400

    echo body to console

--- app.js
+++ app.js
@@ -123,6 +123,10 @@ app.post('/reg', function (req, res) {
 
 	console.log("the /reg email is: " + req.body.email)
 
+	console.dir(req)
+
+	// body = JSON.parse()
+
 	var options = {
 		method: 'POST',
 	  url: 'https://okta-jumio.oktapreview.com/api/v1/users',
